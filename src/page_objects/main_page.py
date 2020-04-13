adf8a5fb96a85ccd4c14c99dfbd2daafc7f2b1f2
lucasstd <lucas.std1@gmail.com>
2020-04-12 23:49:50 -0300

    add page objects

---
+++ src/page_objects/main_page.py
@@ -0,0 +1,12 @@
+from ..settings import configs
+
+class MainPage:
+    url = configs['BASE_URL']
+    input_quanto_tempo = "#tempo"
+    input_valor_aplicar = "#valorAplicar"
+    input_valor_investir = "#valorInvestir"
+    drop_num_meses = '''//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/a'''
+    btn_simular = '''//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button'''
+    radio_btn_empresa = '''//*[@id=\"formInvestimento\"]/div[1]/input[2]'''
+    table = '''/html/body/div[3]/div/div/div[1]/div/div[2]/div[1]/table'''
+    btn_repeat_simulation = '''/html/body/div[3]/div/div/div[1]/div/div[2]/a'''
