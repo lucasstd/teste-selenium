7c2184348929183028cf0185c6eba586e8788616
lucasstd <lucas.std1@gmail.com>
2020-04-13 01:16:29 -0300

    add errors messages in page objects

--- src/page_objects/main_page.py
+++ src/page_objects/main_page.py
@@ -8,5 +8,10 @@ class MainPage:
     drop_num_meses = '''//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/a'''
     btn_simular = '''//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button'''
     radio_btn_empresa = '''//*[@id=\"formInvestimento\"]/div[1]/input[2]'''
+    # pagina da tabela
     table = '''/html/body/div[3]/div/div/div[1]/div/div[2]/div[1]/table'''
     btn_repeat_simulation = '''/html/body/div[3]/div/div/div[1]/div/div[2]/a'''
+    # mensagens de erro
+    tempo_error = "#tempo-error"
+    valorAplicar_error = "#valorAplicar-error"
+    valorInvestir_error = "#valorInvestir-error"
