56c32240cde644b0ebef7bbaa31a7bcc69915e47
lucasstd <lucas.std1@gmail.com>
2020-04-13 01:17:21 -0300

    add high val test to test cases

---
+++ src/page_tests/test_high_val.py
@@ -0,0 +1,18 @@
+from seleniumbase import BaseCase
+from ..page_objects.main_page import MainPage as PageObjects
+
+
+class LowValuesTest(BaseCase):
+
+    def test_low_values(self):
+        self.open(PageObjects.url)
+        # Preenche valor para aplicar com 2,00
+        self.update_text(PageObjects.input_valor_aplicar, '999999999999999999999999,00')
+        # Preenche valor que você quer poupar com 20,00
+        self.update_text(PageObjects.input_valor_investir, '999999999999999999999999,00')
+        # Por quanto tempo você quer poupar com 20
+        self.update_text(PageObjects.input_quanto_tempo, '9')
+        self.click(PageObjects.btn_simular)
+        # Verifica se as mensagens de erro aparecem
+        self.assert_element(PageObjects.valorAplicar_error)
+        self.assert_element(PageObjects.valorInvestir_error)
