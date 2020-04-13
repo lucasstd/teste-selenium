5b869dd79ad3b168cec0ecf51c1bce4a58df1662
lucasstd <lucas.std1@gmail.com>
2020-04-13 01:17:09 -0300

    add actions to correct test case

--- src/page_tests/test_correct_data.py
+++ src/page_tests/test_correct_data.py
@@ -4,14 +4,21 @@ from ..page_objects.main_page import MainPage as PageObjects
 
 class HappyPathTest(BaseCase):
 
-    def test_google_dot_com(self):
-        self.open(PageObjects.url)
+    def common_actions(self):
         # Preenche valor para aplicar com 20,00
         self.update_text(PageObjects.input_valor_aplicar, '20,00')
         # Preenche valor que você quer poupar com 20,00
         self.update_text(PageObjects.input_valor_investir, '20,00')
         # Por quanto tempo você quer poupar com 20
         self.update_text(PageObjects.input_quanto_tempo, '20')
+
         self.click(PageObjects.btn_simular)
         self.assert_element(PageObjects.table)
         self.click(PageObjects.btn_repeat_simulation)
+
+    def test_happy_path_para_voce(self):
+        self.open(PageObjects.url)
+        self.common_actions()
+        # Testar para empresa
+        self.click(PageObjects.radio_btn_empresa)
+        self.common_actions()
